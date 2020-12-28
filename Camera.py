a5d0009701b94d62265d416ae5d3c827764273c4
Jordan Marshall <60512350+PlantEcology@users.noreply.github.com>
2020-12-28 14:01:10 -0500

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -13,4 +13,4 @@ imgTime = today("%Y%m%d-%H%M%S")
 camera.resolution = (3280, 2464)
 camera.rotation = 90
 camera.capture("/home/pi/Desktop/img%s.jpg" $ imgTime)
-camera.stop_preview()
+#camera.stop_preview()
